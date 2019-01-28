0aab82f6fa91508b08083d64f9dcec6b9eed358a
Gu Sul <guscandar@gmail.com>
2019-01-28 09:44:21 +0900

    Initial Commit

---
+++ FreeTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeTalk
+//
+//  Created by 설구 on 28/01/2019.
+//  Copyright © 2019 sulgosu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
